eab22dd7abe72e21f2e9138c173783b29b996ad5
RoizHsu <qqa123456408@gmail.com>
2025-12-22 14:49:41 +0800

    顯示公告中文名稱

--- title_announcement/apps.py
+++ title_announcement/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class TitleAnnouncementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'title_announcement'
+    verbose_name = '公告管理'
